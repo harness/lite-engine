94462b446cb53587abe6a0c7fb91a950e1dfa50a
Jamie Li <tianyi.li@harness.io>
2022-10-24 14:36:21 -0400

    [CI-3333]: SBT support (#91)

---
+++ ti/instrumentation/java/sbt.go
@@ -0,0 +1,67 @@
+// Copyright 2022 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Polyform License
+// that can be found in the LICENSE file.
+
+package java
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/harness/lite-engine/internal/filesystem"
+	"github.com/harness/lite-engine/ti"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	sbtCmd = "sbt"
+)
+
+type sbtRunner struct {
+	fs  filesystem.FileSystem
+	log *logrus.Logger
+}
+
+func NewSBTRunner(log *logrus.Logger, fs filesystem.FileSystem) *sbtRunner { //nolint:revive
+	return &sbtRunner{
+		fs:  fs,
+		log: log,
+	}
+}
+
+func (m *sbtRunner) AutoDetectPackages(workspace string) ([]string, error) {
+	return DetectPkgs(workspace, m.log, m.fs)
+}
+
+func (m *sbtRunner) GetCmd(ctx context.Context, tests []ti.RunnableTest, userArgs, workspace,
+	agentConfigPath, agentInstallDir string, ignoreInstr, runAll bool) (string, error) {
+	if ignoreInstr {
+		return fmt.Sprintf("%s %s 'test'", sbtCmd, userArgs), nil
+	}
+	javaAgentPath := filepath.Join(agentInstallDir, JavaAgentJar)
+	agentArg := fmt.Sprintf(AgentArg, javaAgentPath, agentConfigPath)
+	instrArg := fmt.Sprintf("'set javaOptions ++= Seq(\"%s\")'", agentArg)
+	defaultCmd := fmt.Sprintf("%s %s %s 'test'", sbtCmd, userArgs, instrArg) // run all the tests
+
+	if runAll {
+		// Run all the tests
+		return defaultCmd, nil
+	}
+	if len(tests) == 0 {
+		return fmt.Sprintf("echo \"Skipping test run, received no tests to execute\""), nil
+	}
+	// Use only unique classes
+	testsList := []string{}
+	set := make(map[string]interface{})
+	for _, t := range tests {
+		if _, ok := set[t.Class]; ok {
+			// The class has already been added
+			continue
+		}
+		set[t.Class] = struct{}{}
+		testsList = append(testsList, t.Pkg+"."+t.Class)
+	}
+	return fmt.Sprintf("%s %s %s 'testOnly %s'", sbtCmd, userArgs, instrArg, strings.Join(testsList, " ")), nil
+}
