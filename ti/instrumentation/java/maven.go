6c8682461065076a28fa30257c54114b7802f30b
Rutvij Mehta <rutvij.mehta@harness.io>
2022-11-03 04:52:26 +0530

    (feat) Test Autodetect support for Scala and Kotlin

--- ti/instrumentation/java/maven.go
+++ ti/instrumentation/java/maven.go
@@ -39,21 +39,13 @@ func (m *mavenRunner) AutoDetectPackages(workspace string) ([]string, error) {
 // AutoDetectTests parses all the Java test files and converts them to RunnableTest
 func (m *mavenRunner) AutoDetectTests(ctx context.Context, workspace string) ([]ti.RunnableTest, error) {
 	tests := make([]ti.RunnableTest, 0)
-	files, _ := getFiles(fmt.Sprintf("%s/**/*.java", workspace))
-	for _, path := range files {
-		if path == "" {
-			continue
-		}
-		node, _ := ParseJavaNode(path)
-		if node.Type != NodeType_TEST {
-			continue
-		}
-		test := ti.RunnableTest{
-			Pkg:   node.Pkg,
-			Class: node.Class,
-		}
-		tests = append(tests, test)
-	}
+	javaTests := GetJavaTests(workspace)
+	scalaTests := GetScalaTests(workspace)
+	kotlinTests := GetKotlinTests(workspace)
+
+	tests = append(tests, javaTests...)
+	tests = append(tests, scalaTests...)
+	tests = append(tests, kotlinTests...)
 	return tests, nil
 }
 
