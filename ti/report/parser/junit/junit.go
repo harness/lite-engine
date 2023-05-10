90c87e02aaed2d3c05d06d2068f5c97b096ebf6c
Anurag Madnawat <anurag.madnawat@harness.io>
2023-05-10 08:55:10 -0700

    [fix]: [CI-7731]: Fix testcase count

--- ti/report/parser/junit/junit.go
+++ ti/report/parser/junit/junit.go
@@ -56,6 +56,7 @@ func processTestSuites(tests *[]*ti.TestCase, suites []gojunit.Suite) int {
 			ct := convert(test, suite)
 			if ct.Name != "" {
 				*tests = append(*tests, ct)
+				totalTests += 1
 			}
 		}
 		totalTests += processTestSuites(tests, suite.Suites)
