9749a192c31cc4a75ae94282f1321ff29f30284f
Anurag Madnawat <anurag.madnawat@harness.io>
2023-07-18 14:21:01 -0700

    Update utils.go

--- ti/instrumentation/utils.go
+++ ti/instrumentation/utils.go
@@ -198,7 +198,7 @@ func getSplitTests(ctx context.Context, log *logrus.Logger, testsToSplit []ti.Ru
 
 // getChangedFiles returns a list of files changed in the PR along with their corresponding status
 func getChangedFiles(ctx context.Context, workspace, lastSuccessfulCommitID string, isPushTrigger bool, log *logrus.Logger) ([]ti.File, error) {
-	diffFilesCmdFinal := diffFilesCmd sdf
+	diffFilesCmdFinal := diffFilesCmd
 	if isPushTrigger {
 		diffFilesCmdPushTrigger = append(diffFilesCmdPushTrigger, lastSuccessfulCommitID)
 		diffFilesCmdFinal = diffFilesCmdPushTrigger
