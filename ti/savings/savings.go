1e708f7a59b6734441c7dad499bbf546c9d5135e
Rutvij Mehta <rutvij.mehta@harness.io>
2024-01-29 22:31:36 -0800

    Add logging

--- ti/savings/savings.go
+++ ti/savings/savings.go
@@ -2,6 +2,8 @@ package savings
 
 import (
 	"context"
+	"strconv"
+	"time"
 
 	tiCfg "github.com/harness/lite-engine/ti/config"
 	"github.com/harness/lite-engine/ti/savings/cache"
@@ -11,9 +13,18 @@ import (
 
 func ParseAndUploadSavings(ctx context.Context, workspace string, log *logrus.Logger, stepID string, tiConfig *tiCfg.Cfg) {
 	// Cache Savings
-	state, time, err := cache.ParseCacheSavings(workspace, log)
+	start := time.Now()
+	state, timeTaken, err := cache.ParseCacheSavings(workspace, log)
 	if err == nil {
-		_ = tiConfig.GetClient().WriteSavings(ctx, stepID, types.BUILD_CACHE, state, int64(time))
+		log.Infof("Succesfully parsed savings with state %s and time %sms in %0.2f seconds",
+			string(state), strconv.Itoa(timeTaken), time.Since(start).Seconds())
+
+		tiStart := time.Now()
+		tiErr := tiConfig.GetClient().WriteSavings(ctx, stepID, types.BUILD_CACHE, state, int64(timeTaken))
+		if tiErr == nil {
+			log.Infof("Successfully uploaded savings for feature %s in %0.2f seconds",
+				types.BUILD_CACHE, time.Since(tiStart).Seconds())
+		}
 	}
 	// DLC Savings (Placeholder)
 	// Cache Intel savings (Placeholder)
