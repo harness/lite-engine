4b9ca819fec6a7b379967fe7efe2179f89abd566
Anurag Madnawat <anurag.madnawat@harness.io>
2024-03-12 10:03:02 -0700

    Update runtestsV2.go

--- pipeline/runtime/runtestsV2.go
+++ pipeline/runtime/runtestsV2.go
@@ -40,7 +40,8 @@ const (
 )
 
 // Ignoring optimization state for now
-func executeRunTestsV2Step(ctx context.Context, engine *engine.Engine, r *api.StartStepRequest, out io.Writer, tiConfig *tiCfg.Cfg) (*runtime.State, map[string]string, map[string]string, []byte, []*api.OutputV2, string, error) { //nolint:funlen,gocritic,gocyclo
+func executeRunTestsV2Step(ctx context.Context, engine *engine.Engine, r *api.StartStepRequest, out io.Writer,
+	tiConfig *tiCfg.Cfg) (*runtime.State, map[string]string, map[string]string, []byte, []*api.OutputV2, string, error) { //nolint:funlen,gocritic,gocyclo
 	start := time.Now()
 	tmpFilePath := tiConfig.GetDataDir()
 	fs := filesystem.New()
