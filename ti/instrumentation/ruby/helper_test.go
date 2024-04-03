c4092176944d25035a4f42f2fbd34f43032cade0
Anurag Madnawat <anurag.madnawat@harness.io>
2024-04-03 14:12:02 -0500

    Update helper_test.go

--- ti/instrumentation/ruby/helper_test.go
+++ ti/instrumentation/ruby/helper_test.go
@@ -1,6 +1,7 @@
 package ruby
 
 import (
+	"reflect"
 	"testing"
 
 	ti "github.com/harness/ti-client/types"
@@ -51,4 +52,44 @@ func TestGetRubyTestsFromPatternPositiveNoVendorIgnore(t *testing.T) {
 	assert.NotNil(t, tests)
 	assert.Len(t, tests, 3)
 	assert.IsType(t, []ti.RunnableTest{}, tests)
+}
+
+func TestGetRubyGlobs(t *testing.T) {
+	// Test case 1: testGlobs is empty, envs does not contain "TI_SKIP_EXCLUDE_VENDOR"
+	testGlobs := []string{}
+	envs := map[string]string{}
+	includeGlobs, excludeGlobs := GetRubyGlobs(testGlobs, envs)
+	expectedIncludeGlobs := defaultTestGlobs
+	expectedExcludeGlobs := filterExcludeGlobs
+	if !reflect.DeepEqual(includeGlobs, expectedIncludeGlobs) || !reflect.DeepEqual(excludeGlobs, expectedExcludeGlobs) {
+		t.Errorf("Test case 1 failed. Expected include globs: %v, got: %v. Expected exclude globs: %v, got: %v", expectedIncludeGlobs, includeGlobs, expectedExcludeGlobs, excludeGlobs)
+	}
+
+	// Test case 2: testGlobs is not empty, envs does not contain "TI_SKIP_EXCLUDE_VENDOR"
+	testGlobs = []string{"test1.rb", "test2.rb"}
+	includeGlobs, excludeGlobs = GetRubyGlobs(testGlobs, envs)
+	expectedIncludeGlobs = testGlobs
+	expectedExcludeGlobs = filterExcludeGlobs
+	if !reflect.DeepEqual(includeGlobs, expectedIncludeGlobs) || !reflect.DeepEqual(excludeGlobs, expectedExcludeGlobs) {
+		t.Errorf("Test case 2 failed. Expected include globs: %v, got: %v. Expected exclude globs: %v, got: %v", expectedIncludeGlobs, includeGlobs, expectedExcludeGlobs, excludeGlobs)
+	}
+
+	// Test case 3: testGlobs is empty, envs contains "TI_SKIP_EXCLUDE_VENDOR" set to "true"
+	testGlobs = []string{}
+	envs["TI_SKIP_EXCLUDE_VENDOR"] = "true"
+	includeGlobs, excludeGlobs = GetRubyGlobs(testGlobs, envs)
+	expectedIncludeGlobs = defaultTestGlobs
+	expectedExcludeGlobs = []string{}
+	if !reflect.DeepEqual(includeGlobs, expectedIncludeGlobs) || !reflect.DeepEqual(excludeGlobs, expectedExcludeGlobs) {
+		t.Errorf("Test case 3 failed. Expected include globs: %v, got: %v. Expected exclude globs: %v, got: %v", expectedIncludeGlobs, includeGlobs, expectedExcludeGlobs, excludeGlobs)
+	}
+
+	// Test case 4: testGlobs is not empty, envs contains "TI_SKIP_EXCLUDE_VENDOR" set to "true"
+	testGlobs = []string{"test1.rb", "test2.rb"}
+	includeGlobs, excludeGlobs = GetRubyGlobs(testGlobs, envs)
+	expectedIncludeGlobs = testGlobs
+	expectedExcludeGlobs = []string{}
+	if !reflect.DeepEqual(includeGlobs, expectedIncludeGlobs) || !reflect.DeepEqual(excludeGlobs, expectedExcludeGlobs) {
+		t.Errorf("Test case 4 failed. Expected include globs: %v, got: %v. Expected exclude globs: %v, got: %v", expectedIncludeGlobs, includeGlobs, expectedExcludeGlobs, excludeGlobs)
+	}
 }
