f922323e4463a491f929adfddaa7e7ceebc328c0
smjt-h <soumyajit.das@harness.io>
2024-04-08 14:11:05 +0530

    [CI-11492]: fix autodetection splitting ruby

--- ti/instrumentation/ruby/helper.go
+++ ti/instrumentation/ruby/helper.go
@@ -69,6 +69,8 @@ func GetRubyTests(workspace string, testGlobs []string, excludeGlobs []string, l
 		testGlobs = defaultTestGlobs
 	}
 	log.Infoln(fmt.Sprintf("testGlobs: %v", testGlobs))
+	log.Infoln(fmt.Sprintf("workspace: %v", workspace))
+
 	tests := getRubyTestsFromPattern(workspace, testGlobs, excludeGlobs, log)
 
 	if len(tests) == 0 {
