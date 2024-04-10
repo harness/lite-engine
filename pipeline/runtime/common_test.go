fb9a392ee1d5dd2c8d84e29bf7d0b6c26ed6be1e
sahithiharness <sahithi.banda@harness.io>
2024-04-10 12:47:36 +0530

    added unit test

--- pipeline/runtime/common_test.go
+++ pipeline/runtime/common_test.go
@@ -32,6 +32,36 @@ func TestFetchExportedVars(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.Name, func(t *testing.T) {
+			envMap, err := fetchExportedVarsFromEnvFile(tc.OutputFile, os.Stdout, false)
+			assert.Equal(t, tc.EnvMap, envMap)
+			assert.Equal(t, tc.Error, err)
+		})
+	}
+}
+
+func TestExportedVarsWithNewVersionOfGodotEnv(t *testing.T) {
+	tests := []struct {
+		Name       string
+		OutputFile string
+		EnvMap     map[string]string
+		Error      error
+	}{
+		{
+			Name:       "env_variable_value_with_#",
+			OutputFile: "testdata/value_with_hash.txt",
+			EnvMap:     map[string]string{"VALUE_WITH_HASH": "value#123"},
+			Error:      nil,
+		},
+		{
+			Name:       "env_variable_with_-",
+			OutputFile: "testdata/variable_with_dash.txt",
+			EnvMap:     map[string]string{"VARIABLE_WITH_DASH-VARIABLE": "value"},
+			Error:      nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
 			envMap, err := fetchExportedVarsFromEnvFile(tc.OutputFile, os.Stdout, true)
 			assert.Equal(t, tc.EnvMap, envMap)
 			assert.Equal(t, tc.Error, err)
