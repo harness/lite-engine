c193f8c8009855f703cda9c74146119691bc668d
Anurag Madnawat <anurag.madnawat@harness.io>
2024-07-25 10:16:47 -0700

    [feat]: [CI-13579]: Return  Optimization state for Failed Step (#303)
    * [feat]: [CI-13579]: Return  Optimization state for Failed Step
    
    * Update savings.go
    
    * Update savings.go
    
    * Update savings.go

--- pipeline/runtime/step_executor.go
+++ pipeline/runtime/step_executor.go
@@ -451,5 +451,5 @@ func convertPollResponse(r *api.PollStepResponse) api.VMTaskExecutionResponse {
 	if r.Error == "" {
 		return api.VMTaskExecutionResponse{CommandExecutionStatus: api.Success, OutputVars: r.Outputs, Artifact: r.Artifact, Outputs: r.OutputV2, OptimizationState: r.OptimizationState}
 	}
-	return api.VMTaskExecutionResponse{CommandExecutionStatus: api.Failure, ErrorMessage: r.Error}
+	return api.VMTaskExecutionResponse{CommandExecutionStatus: api.Failure, ErrorMessage: r.Error, OptimizationState: r.OptimizationState}
 }
