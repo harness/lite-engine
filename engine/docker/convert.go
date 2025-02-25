6c5aa34c4edf153fe200e049caf4c75f5f5af285
Vinícius Calasans <vinicius.calasans@harness.io>
2025-02-25 12:12:35 +0000

    fix: [PIPE-25062]: Resolve symlinks when parsing the host paths of docker volumes to bind (#335)
    * Update comment
    * Resolve symlinks when parsing the host paths of docker volumes to bind

--- engine/docker/convert.go
+++ engine/docker/convert.go
@@ -9,6 +9,7 @@
 package docker
 
 import (
+	"path/filepath"
 	"strings"
 
 	"github.com/harness/lite-engine/engine/spec"
@@ -206,7 +207,18 @@ func toVolumeSlice(pipelineConfig *spec.PipelineConfig, step *spec.Step) []strin
 			to = append(to, path)
 		}
 		if isBindMount(volume) {
-			path := volume.HostPath.Path + ":" + mount.Path
+			// Here, we attempt to convert any possible symlinks to the actual folder it is pointing to,
+			// in order to avoid any issues with vm's filesystem not in sync with symlinks in the host.
+			// Known case: Rancher Desktop, in MacOS, uses lima-vm, and the `/tmp` folder in the vm is
+			// not the same as the `/tmp` folder of the host (which is a symlink pointing to "/private/tmp").
+			// By resolving the symlink here, `/tmp` will be converted to its resolved folder, `/private/tmp`,
+			// and the bind mount will work as expected.
+			hostPath, err := filepath.EvalSymlinks(volume.HostPath.Path)
+			if err != nil {
+				// If failed to resolve symlinks, use the original hostPath.
+				hostPath = volume.HostPath.Path
+			}
+			path := hostPath + ":" + mount.Path
 			to = append(to, path)
 		}
 	}
