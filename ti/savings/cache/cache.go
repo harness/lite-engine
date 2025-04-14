53791ebb727deb995eb5f479c463ee8219f6cd85
Soumyajit Das <soumyajit.das@harness.io>
2025-04-14 11:54:57 -0700

    fix: [CI-16878]: Add maven cache savings report

--- ti/savings/cache/cache.go
+++ ti/savings/cache/cache.go
@@ -2,8 +2,10 @@ package cache
 
 import (
 	"github.com/harness/lite-engine/ti/savings/cache/gradle"
+	"github.com/harness/lite-engine/ti/savings/cache/maven"
 	"github.com/harness/ti-client/types"
 	gradleTypes "github.com/harness/ti-client/types/cache/gradle"
+	mavenTypes "github.com/harness/ti-client/types/cache/maven"
 	"github.com/sirupsen/logrus"
 )
 
@@ -16,5 +18,10 @@ func ParseCacheSavings(workspace string, log *logrus.Logger) (types.Intelligence
 		return cacheState, 0, savingsRequest, err
 	}
 	savingsRequest.GradleMetrics = gradleTypes.Metrics{Profiles: profiles}
+	cacheState, reports, err := maven.ParseSavings(workspace, log)
+	savingsRequest.MavenMetrics = mavenTypes.MavenMetrics{Reports: reports}
+	if err != nil {
+		return cacheState, 0, savingsRequest, err
+	}
 	return cacheState, buildTime, savingsRequest, nil
 }
