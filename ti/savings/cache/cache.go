d19d444e8afcee379a1338083f4e5ec2cc25673d
Anurag Madnawat <anurag.madnawat@harness.io>
2025-05-02 20:56:26 +0000

    [feat]: [CI-17409]: Replace errors.Join due to no support in go1.19 (#353)
    * Replace errors.Join due to no support in go1.19
    
    Signed-off-by: Anurag Madnawat <anurag.madnawat@harness.io>

--- ti/savings/cache/cache.go
+++ ti/savings/cache/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/harness/lite-engine/ti/savings/cache/gradle"
 	"github.com/harness/lite-engine/ti/savings/cache/maven"
@@ -10,6 +11,19 @@ import (
 	mavenTypes "github.com/harness/ti-client/types/cache/maven"
 	"github.com/sirupsen/logrus"
 )
+
+func joinErrors(errs ...error) error {
+	var messages []string
+	for _, err := range errs {
+		if err != nil {
+			messages = append(messages, err.Error())
+		}
+	}
+	if len(messages) == 0 {
+		return nil
+	}
+	return errors.New(strings.Join(messages, "; "))
+}
 
 func ParseCacheSavings(workspace string, log *logrus.Logger) (types.IntelligenceExecutionState, int, types.SavingsRequest, error) {
 	savingsRequest := types.SavingsRequest{}
@@ -21,7 +35,7 @@ func ParseCacheSavings(workspace string, log *logrus.Logger) (types.Intelligence
 	mavenCacheState, reports, mavenErr := maven.ParseSavings(workspace, log)
 
 	if gradleErr != nil && mavenErr != nil {
-		return types.FULL_RUN, 0, savingsRequest, errors.Join(gradleErr, mavenErr)
+		return types.FULL_RUN, 0, savingsRequest, joinErrors(gradleErr, mavenErr)
 	}
 
 	if mavenCacheState == types.OPTIMIZED {
