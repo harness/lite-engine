67936f804d6475cc0beb7f152a2b464a8db93c12
ShobhitSingh11 <shobhit.singh@harness.io>
2025-08-05 16:25:04 +0530

    feat: [PIPE-28834]: Permission change

--- engine/engine.go
+++ engine/engine.go
@@ -313,9 +313,10 @@ func createFiles(paths []*spec.File) error {
 			continue
 		}
 
+		// For creating directories if not exists
 		dir := filepath.Dir(path)
 		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err := os.MkdirAll(dir, fs.FileMode(f.Mode)); err != nil {
+			if err := os.MkdirAll(dir, fs.FileMode(permissions)); err != nil {
 				return errors.Wrap(err, fmt.Sprintf("failed to create directory: for path %q", path))
 			}
 		}
