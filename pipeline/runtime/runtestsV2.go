6be0c62183945a0b826315aa62aaeba4b1e8ba29
Ankush Chatterjee <ankush.chatterjee@harness.io>
2025-08-05 23:37:47 +0530

    Introduce FF: CI_TI_RERUN_FAILED_TEST

--- pipeline/runtime/runtestsV2.go
+++ pipeline/runtime/runtestsV2.go
@@ -52,6 +52,7 @@ const (
 	dotNetAgentV2Path       = "/dotnet/v2/"
 	dotNetConfigV2Dir       = "%s/ti/v2/dotnet/config"
 	javascriptRequireFile   = "ti-agent.cjs"
+	ciTiRerunFailedTestFF   = "CI_TI_RERUN_FAILED_TEST_FF"
 )
 
 //nolint:gocritic,gocyclo,funlen
@@ -314,6 +315,7 @@ func getTestsSelectionWithTiModeEnabled(ctx context.Context, fs filesystem.FileS
 
 	filesWithpkg := java.ReadPkgs(log, fs, workspace, files)
 	selection, err = instrumentation.SelectTests(ctx, workspace, filesWithpkg, runOnlySelectedTests, stepID, testGlobs, fs, tiConfig)
+	selection = instrumentation.FilterPreviousFailures(selection, envs)
 	if err != nil {
 		log.WithError(err).Errorln("An unexpected error occurred during test selection. Running all tests.")
 		runOnlySelectedTests = false
@@ -774,6 +776,7 @@ func collectTestReportsAndCg(
 	tests, _ := collectTestReports(ctx, log, r, stepName, tiConfig, telemetryData)
 
 	testFailed := false
+	rerunFailedTests := false
 
 	if envValue, ok := envs["DISABLE_CG_UPLOAD_ON_FAILURE_FF"]; ok {
 		if envValue == "true" && tests != nil {
@@ -786,8 +789,14 @@ func collectTestReportsAndCg(
 		}
 	}
 
+	if envValue, ok := envs[rerunFailedTestsFF]; ok {
+		if envValue == "true" {
+			rerunFailedTests = true
+		}
+	}
+
 	cgStart := time.Now()
-	cgErr := collectCgFn(ctx, stepName, time.Since(start).Milliseconds(), log, cgStart, tiConfig, outDir, r.ID, testFailed, tests)
+	cgErr := collectCgFn(ctx, stepName, time.Since(start).Milliseconds(), log, cgStart, tiConfig, outDir, r.ID, testFailed, tests, rerunFailedTests)
 	if cgErr != nil {
 		log.WithField("error", cgErr).Errorln(fmt.Sprintf("Unable to collect callgraph. Time taken: %s", time.Since(cgStart)))
 		cgErr = fmt.Errorf("failed to collect callgraph: %s", cgErr)
