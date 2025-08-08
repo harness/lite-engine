50f7dd1bae52569d3c967ef63209227373b864c8
abhay084 <abhay.ganvir@harness.io>
2025-08-08 14:58:20 +0530

    "feat:[CI-13780]: Added support for masking (#367)"
    This reverts commit 00df24a2ef76aa48ad35e21840d5c4a5f0f36de5.

--- pipeline/runtime/step_executor.go
+++ pipeline/runtime/step_executor.go
@@ -413,7 +413,7 @@ func getLogStreamWriter(r *api.StartStepRequest) logstream.Writer {
 	client := pipelineState.GetLogStreamClient()
 
 	wc := livelog.New(client, r.LogKey, r.Name, getNudges(), false, pipelineState.GetLogConfig().TrimNewLineSuffix, pipelineState.GetLogConfig().SkipOpeningStream)
-	wr := logstream.NewReplacerWithEnvs(wc, secrets, r.Envs)
+	wr := logstream.NewReplacer(wc, secrets)
 	go wr.Open() //nolint:errcheck
 	return wr
 }
