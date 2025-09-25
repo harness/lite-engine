03b63219456e551f3400f756816a84106f96b8d5
Vinicius Calasans <vinicius.calasans@harness.io>
2025-09-24 21:29:15 -0700

    Fix level

--- engine/exec/exec_darwin.go
+++ engine/exec/exec_darwin.go
@@ -80,7 +80,7 @@ func waitOnProcessGroup(ctx context.Context, cmd *exec.Cmd, cmdSignal chan<- cmd
 	}
 	// Collect group leader's exit status
 	waitForCmd(cmd, cmdSignal)
-	logrus.WithContext(ctx).Errorf("all processes in process group %d exited", pgid)
+	logrus.WithContext(ctx).Debugf("all processes in process group %d exited", pgid)
 }
 
 // Waits for a process to terminate but does *not* collect its exit status,
