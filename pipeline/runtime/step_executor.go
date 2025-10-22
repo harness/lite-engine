914999524239c99104d227ecf817d1ec82346da4
Devansh  Mathur <devanshmathur@harness.io>
2025-10-22 05:47:08 +0000

    feat: [CI-19005]: Updating LE for Pipeline Annotations. (#449)
    * c09478 feat: [CI-19005]: Updating LE for Pipeline Annotations.

--- pipeline/runtime/step_executor.go
+++ pipeline/runtime/step_executor.go
@@ -93,6 +93,13 @@ func (e *StepExecutor) StartStep(ctx context.Context, r *api.StartStepRequest) e
 		state, outputs, envs, artifact, outputV2, telemetrydata, optimizationState, stepErr := e.executeStep(r, wr)
 		status := StepStatus{Status: Complete, State: state, StepErr: stepErr, Outputs: outputs, Envs: envs,
 			Artifact: artifact, OutputV2: outputV2, OptimizationState: optimizationState, TelemetryData: telemetrydata}
+
+		ffEnabled := isAnnotationsEnabled(r.Envs)
+		if state.ExitCode == 0 && ffEnabled {
+			logrus.WithContext(ctx).WithField("id", r.ID).Infoln("ANNOTATIONS: scheduling annotations post")
+			go e.postAnnotationsToPipeline(context.Background(), r)
+		}
+
 		e.mu.Lock()
 		e.stepStatus[r.ID] = status
 		channels := e.stepWaitCh[r.ID]
@@ -131,6 +138,12 @@ func (e *StepExecutor) StartStepWithStatusUpdate(ctx context.Context, r *api.Sta
 			status := StepStatus{Status: Complete, State: state, StepErr: stepErr, Outputs: outputs, Envs: envs,
 				Artifact: artifact, OutputV2: outputV2, OptimizationState: optimizationState, TelemetryData: telemetryData}
 			pollResponse := convertStatus(status)
+			ffEnabled := isAnnotationsEnabled(r.Envs)
+			if state.ExitCode == 0 && ffEnabled {
+				logrus.WithContext(ctx).WithField("id", r.ID).Infoln("ANNOTATIONS: scheduling annotations post")
+				go e.postAnnotationsToPipeline(context.Background(), r)
+			}
+
 			if r.StageRuntimeID != "" && len(pollResponse.Envs) > 0 {
 				pipeline.GetEnvState().Add(r.StageRuntimeID, pollResponse.Envs)
 			}
