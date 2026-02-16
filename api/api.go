518fba237b09c3bb7403f5075f4a52ab02e49fce
Gargi <gargi@harness.io>
2026-02-16 18:25:29 +0530

    feat: [CI-20593]: Cleanup CI_PARSE_SAVINGS FF - Remove parseSavings from API and config
    - Removed ParseSavings field from TIConfig API struct
    - Removed parseSavings field from Cfg struct
    - Removed parseSavings parameter from New() constructor
    - Updated all callers to not pass parseSavings parameter
    
    Savings parsing is now always enabled by default.

--- api/api.go
+++ api/api.go
@@ -235,7 +235,6 @@ type (
 		TargetBranch string `json:"target_branch,omitempty"`
 		CommitBranch string `json:"commit_branch,omitempty"`
 		CommitLink   string `json:"commit_link,omitempty"`
-		ParseSavings bool   `json:"parse_savings,omitempty"`
 	}
 
 	// AnnotationsConfig carries configuration for posting annotations to Pipeline Service
