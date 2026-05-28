87786758dd6f48e78ce2d310d125d9dd8d85212a
Kiruthika Meena Ravichandran <meena.ravichandran@harness.io>
2026-05-27 20:24:26 -0400

    fix deadlock

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -9,7 +9,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
+	"log"
 	"regexp"
 	"strings"
 	"sync"
@@ -298,7 +298,7 @@ func (b *Writer) flush() error {
 		thresholdTime := time.Now().Add(-flushThresholdTime)
 		if b.lastFlushTime.Before(thresholdTime) {
 			if err := osstats.DumpProcessInfo(); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to dump process info: %v\n", err)
+				log.Printf("failed to dump process info: %v", err)
 			}
 			// reset lastFlushTime if stats were dumped
 			b.lastFlushTime = time.Now()
@@ -309,7 +309,7 @@ func (b *Writer) flush() error {
 	b.lastFlushTime = time.Now()
 	err := b.client.Write(b.ctx, b.key, lines)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to flush lines: key=%s num_lines=%d err=%v\n", b.key, len(lines), err)
+		log.Printf("failed to flush lines: key=%s num_lines=%d err=%v", b.key, len(lines), err)
 		return err
 	}
 	return nil
