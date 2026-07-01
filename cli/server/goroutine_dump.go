fa3c7d1e87c080c93c12c82e631359be8de73852
Avijeet <avijeet.singhmandloi@harness.io>
2026-07-02 00:33:27 +0530

    fix test failures

--- cli/server/goroutine_dump.go
+++ cli/server/goroutine_dump.go
@@ -13,13 +13,18 @@ import (
 	"time"
 )
 
-const defaultGoroutineDumpDir = "/tmp"
+const (
+	defaultGoroutineDumpDir = "/tmp"
+	goroutineDumpDirPerm    = 0700
+	goroutineDumpFilePerm   = 0600
+	goroutineDumpDebugLevel = 2
+)
 
 func writeGoroutineDump(dir string, now time.Time) (string, error) {
 	if dir == "" {
 		dir = defaultGoroutineDumpDir
 	}
-	if err := os.MkdirAll(dir, 0700); err != nil {
+	if err := os.MkdirAll(dir, goroutineDumpDirPerm); err != nil {
 		return "", err
 	}
 
@@ -31,7 +36,7 @@ func writeGoroutineDump(dir string, now time.Time) (string, error) {
 			now.UTC().Format("20060102T150405.000000000Z"),
 		),
 	)
-	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0600)
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, goroutineDumpFilePerm)
 	if err != nil {
 		return "", err
 	}
@@ -41,7 +46,7 @@ func writeGoroutineDump(dir string, now time.Time) (string, error) {
 	if profile == nil {
 		return "", errors.New("goroutine profile is not available")
 	}
-	if err := profile.WriteTo(file, 2); err != nil {
+	if err := profile.WriteTo(file, goroutineDumpDebugLevel); err != nil {
 		return "", err
 	}
 
